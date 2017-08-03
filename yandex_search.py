20a8ebb72bd271c9af4dcfbfbe0e830befe97441
Ilia Kurbatovsky <kurbatovsky@gmail.com>
2017-08-03 14:51:27 +0200

    Story 5: Fix yandex_search.py

--- yandex_search.py
+++ yandex_search.py
@@ -5,6 +5,13 @@ import requests
 import lxml.html
 
 
+def clear_request(params):
+    """ Create request """
+    for param in params:
+        param = re.sub(r'\W', '', param)
+    return params
+
+
 def get_page(params):
     """ Get page from request"""
     url = "https://yandex.ru/search/"
@@ -14,7 +21,7 @@ def get_page(params):
 
 def get_html(params):
     """ Get HTML from web"""
-    page = get_page(params)
+    page = get_page(clear_request(params))
     return page.text
 
 
@@ -25,11 +32,20 @@ def parse_html(html, xpath):
 
 
 if __name__ == "__main__":
-    HTML = get_html(sys.argv[1:])
-    for i in range(1, 4, 1):
-        xPath = "/html/body/div[2]/div[2]/div[3]/div[1]/div[1]/ul/li[{}]/div/h2/a/@href".format(i)
-        link = parse_html(HTML, xPath)
-        if link is []:
-            xPath = re.sub(r"div/", '', xPath)
+    if len(sys.argv) > 1:
+        HTML = get_html(sys.argv[1:])
+        for i in range(1, 4, 1):
+            xPath = "/html/body/div[2]/div[2]/div[3]/div[1]/div[1]/ul/li[{}]/div/h2/a/@href".format(i)
             link = parse_html(HTML, xPath)
-        print(link)
+            try:
+                if link is []:
+                    xPath = re.sub(r"div/", '', xPath)
+                    link = parse_html(HTML, xPath)
+                    print(link[0])
+                else:
+                    print(link[0])
+            except IndexError:
+                print("No results")
+                break
+    else:
+        print("Use format: 'python yandex_search.py *params'")
