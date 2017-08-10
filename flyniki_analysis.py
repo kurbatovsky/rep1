b3f1ddcd08dd6d807110b166d85fba34501e84ed
Ilia Kurbatovsky <kurbatovsky@gmail.com>
2017-08-10 12:50:38 +0300

    Story 21: Fix

--- flyniki_analysis.py
+++ flyniki_analysis.py
@@ -12,32 +12,29 @@ DATA = {'_ajax[templates][]': 'main',
         '_ajax[requestParams][departure]': sys.argv[1],
         '_ajax[requestParams][destination]': sys.argv[2],
         '_ajax[requestParams][outboundDate]': sys.argv[3],
-        '_ajax[requestParams][returnDate]': sys.argv[4],
+        '_ajax[requestParams][returnDate]': sys.argv[4] if len(sys.argv) == 5 else '',
         '_ajax[requestParams][adultCount]': '1',
         '_ajax[requestParams][childCount]': '0',
         '_ajax[requestParams][infantCount]': '0',
         '_ajax[requestParams][returnDeparture]': '',
         '_ajax[requestParams][returnDestination]': '',
         '_ajax[requestParams][openDateOverview]': '',
-        '_ajax[requestParams][oneway]': ''}
+        '_ajax[requestParams][oneway]': '' if len(sys.argv) == 5 else 1}
 
 
 class Flight:
     """ Flight class"""
-    def __init__(self, departure, destination, duration, class_, min_price, max_price, is_back):
+    def __init__(self, departure, destination, duration, class_, min_price, max_price):
         """ Initialization """
-        print("__init__")
-        self.departure = departure
-        self.destination = destination
-        self.duration = duration
+        self.departure = departure.text
+        self.destination = destination.text
+        self.duration = duration.text
         self.class_ = class_
-        self.min_price = min_price
-        self.max_price = max_price
-        self.is_back = is_back
+        self.min_price = min_price.text
+        self.max_price = max_price.text
 
     def show_flight(self):
         """ Show flight in cmd """
-        print("show_flight")
         text = '{0}-{1} {2} {3}-{4} RUB {5}'
         print(text.format(self.departure, self.destination,
                           self.duration, self.min_price,
@@ -46,7 +43,6 @@ class Flight:
 
 def construct_url():
     """ Construct url """
-    print("construct_url")
     url = 'https://www.flyniki.com/ru/booking/flight/vacancy.php?departure={0}&destination' \
           '={1}&outboundDate={2}'
     url_add = '&returnDate={0}'
@@ -59,116 +55,111 @@ def construct_url():
 
 def get_page():
     """ Get page """
-    print("get_page")
     with requests.session() as sess:
         request = sess.get(construct_url())
         post = sess.post(request.url, data=DATA)
         return post
 
 
-def get_html():
+def get_html(page):
     """ Get HTML """
-    print("get_html")
-    html_ = re.sub(r'\\', '', eval(get_page().text)['templates']['main'])
+    html_ = re.sub(r'\\', '', eval(page.text)['templates']['main'])
     return html_
 
 
-def parse_html(xpath):
+def parse_html(html_, xpath):
     """ Parse HTML """
-    print("parse_html")
-    tree = html.fromstring(get_html())
+    tree = html.fromstring(html_)
     return tree.xpath(xpath)
 
 
-def get_info(class_):
+def get_info(html_, class_, back=False):
     """ Get information about flight """
-    print("get_info")
-    # [[Время вылета, время прибытия, время вылета назад, время прибытия назад], ...]
-    # [[Длительность полёта, Длительность полёта назад], ...]
-    # Наименование класса
-    # [[Минимальная цена, Максимальная цена, Минимальная цена назад, Максимальная цена назад], ...]
-    info = {'price': get_price(class_),
-            'time': get_time(),
-            'duration': get_duration(),
+    info = {'minimal': get_prices(html_, class_, back=back, current=False),
+            'current': get_prices(html_, class_, back=back, current=True),
+            'departure': get_time(html_, back=back, destination=False),
+            'destination': get_time(html_, back=back, destination=True),
+            'duration': get_durations(html_, back=back),
             'class': class_}
     return info
 
 
-def get_time():
-    """ Get time from HTML """
-    print("get_time")
-    departure = []
-    for i in range(6):
-        print(i)
-        departure.append(parse_html('//*[@id="flightDepartureFi_{0}"]/time'.format(str(i))))
-    return departure
+def set_direction(back):
+    return 3 if back else 1
 
 
-def get_duration():
+def get_time(html_, back=False, destination=False):
+    """ Get time from HTML"""
+    times = []
+    for i in range(1, 7):
+        times.append(parse_html(html_,
+            '//*[@id="flighttables"]/div[{0}]/div[2]//table[1]/tbody[1]/tr[{1}]/td[2]/span/time[{2}]'.format(
+                set_direction(back), 2*i - 1, 2 if destination else 1)))
+    return times
+
+
+def get_durations(html_, back=False):
     """ Get duration from HTML """
-    print("get_duration")
-    duration = []
-    for i in range(6):
-        print(i)
-        duration.append(parse_html('//*[@id="flightDurationFi_{0}"]'.format(i)))
-    return duration
+    durations = []
+    for i in range(1, 7):
+        durations.append(parse_html(html_,
+            '//*[@id="flighttables"]/div[{0}]/div[2]/table[1]/tbody[1]/tr[{1}]/td[4]/span'.format(
+                set_direction(back), 2*i - 1)))
+    return durations
 
 
-def get_price(class_):
-    """ Get price from HTML """
-    print("get_price")
-    xpath = '//*[@class="fare faregrouptoggle {0} {1} flightfi_{2} bgcolor-{3}"]//span'
-    price = []
-    for i in range(6):
-        print(i)
-        i += 1
-        price.append(parse_html(xpath.format(class_[:3], class_[4:], i, class_)))
-    return price
+def set_class(class_):
+    return 5 if class_ == "ECO_SAVER" else 6 if class_ == "ECO_FLEX" else 7 if class_ == "BUS_SAVER" else 8
 
 
-def info_flight(class_, is_back=False):
+def is_current(current):
+    return 2 if current else 1
+
+
+def get_prices(html_, class_, back=False, current=False):
+    """ Get price fom HTML """
+    xpath = '//*[@id="flighttables"]/div[{0}]/div[2]/table/tbody[1]/tr[{1}]/td[{2}]/label[1]/div[{3}]/span'
+    prices = []
+    for i in range(1, 7):
+        prices.append(parse_html(html_, xpath.format(set_direction(back),
+                                                     2*i - 1,
+                                                     set_class(class_),
+                                                     is_current(current))))
+    return prices
+
+
+def info_flight(info):
     """ Print flights information"""
-    print("info_flight")
-    info = get_info(class_)
-    if not is_back:
-        flying = []
-        for i in range(len(info['time'])):
-            flying.append(Flight(info['time'][i][0].text, info['time'][i][1].text,
-                                 info['duration'][i][0].text, info['class'],
-                                 info['price'][i][0].text, info['price'][i][1].text, False))
-        flying.sort(key=sort_by_time)
-        for fly in flying:
+    for i in range(len(info['departure'])):
+        if info['departure'][i] != [] and info['destination'][i] != [] and info['duration'][i] != [] and info['minimal'][i] != [] and info['current'][i] != []:
+            fly = Flight(info['departure'][i][0],
+                         info['destination'][i][0],
+                         info['duration'][i][0],
+                         info['class'],
+                         info['minimal'][i][0],
+                         info['current'][i][0])
             fly.show_flight()
-    else:
-        flying = []
-        for i in range(len(info['time'])):
-            flying.append(Flight(info['time'][i][2].text, info['time'][i][3].text,
-                                 info['duration'][i][1].text, info['class'],
-                                 info['price'][i][2].text, info['price'][i][3].text, True))
-            flying.sort(key=sort_by_time)
-        for fly in flying:
-            fly.show_flight()
+        else:
+            continue
 
 
 def sort_by_time(flight):
     """ Sort by time """
-    print("sort_by_time")
     return flight.departure
 
 
 def main():
     """ Main function """
-    print("Direct flights:")
-    info_flight("ECO_SAVER")
-    info_flight("ECO_FLEX")
-    info_flight("BUS_SAVER")
-    info_flight("BUS_FLEX")
-    """if len(sys.argv) == 5:
-        print("Return flight:")
-        info_flight("ECO_SAVER", is_back=True)
-        info_flight("ECO_FLEX", is_back=True)
-        info_flight("BUS_SAVER", is_back=True)
-        info_flight("BUS_FLEX", is_back=True)"""
+    html_ = get_html(get_page())
+    info_flight(get_info(html_, class_="ECO_SAVER"))
+    info_flight(get_info(html_, class_="ECO_FLEX"))
+    info_flight(get_info(html_, class_="BUS_SAVER"))
+    info_flight(get_info(html_, class_="BUS_FLEX"))
+    if len(sys.argv) == 5:
+        info_flight(get_info(html_, class_="ECO_SAVER", back=True))
+        info_flight(get_info(html_, class_="ECO_FLEX", back=True))
+        info_flight(get_info(html_, class_="BUS_SAVER", back=True))
+        info_flight(get_info(html_, class_="BUS_FLEX", back=True))
 
 
 if __name__ == "__main__":
