3bbfdbcbd26b7fef2fbfc7df747b561b99fed86b
Ilia Kurbatovsky <kurbatovsky@gmail.com>
2017-08-15 12:21:31 +0300

    Story 30: In Flyniki fix construct_url, get_html and get_line

--- flyniki_analysis.py
+++ flyniki_analysis.py
@@ -2,11 +2,10 @@
 """
 Find flight
 """
-import sys
 import re
 import argparse
-import requests
 from datetime import date
+import requests
 from lxml import html
 
 
@@ -14,13 +13,13 @@ class Parser:
     """ Parser class """
 
     html = ''
-    page = ''
     url = ''
     data = {}
 
     def __init__(self):
         """ Initialization """
         self.args = self.parse_args()
+        self.options = {}
         self.clean_args()
         self.data = {'_ajax[templates][]': 'main',
                      '_ajax[requestParams][departure]': self.args.outbound,
@@ -35,8 +34,7 @@ class Parser:
                      '_ajax[requestParams][openDateOverview]': '',
                      '_ajax[requestParams][oneway]': '' if self.args.return_date != '' else 1}
         self.lines = {'outbound': [], 'return': []}
-        self.construct_url()
-        self.get_page()
+        self.form_options()
         self.get_html()
         self.get_line()
         self.get_line(way='return')
@@ -53,40 +51,41 @@ class Parser:
 
     def clean_args(self):
         """ Clean args """
-        assert re.match(r'^[A-Z][A-Z][A-Z]$', self.args.outbound).group() == self.args.outbound, "IATA must be in AAA format"
-        assert re.match(r'^[A-Z][A-Z][A-Z]$', self.args.return_).group() == self.args.return_, "IATA must be is AAA format"
+        assert re.match(r'^[A-Z][A-Z][A-Z]$',
+                        self.args.outbound).group() == self.args.outbound, "IATA must be in AAA format"
+        assert re.match(r'^[A-Z][A-Z][A-Z]$',
+                        self.args.return_).group() == self.args.return_, "IATA must be is AAA format"
         assert self.is_correct_args(self.args.departure_date), "Incorrect departure date"
         if self.args.return_date != '':
             assert self.is_correct_args(self.args.return_date), "Incorrect return date"
 
     @staticmethod
+    def date_to_int(date_):
+        """ Convert date to int """
+        return int(re.sub(r'-', '', str(date_)))
+
+    @staticmethod
     def is_correct_args(date_):
         """ Check date """
-        return int(re.sub(r'-', '', str(date.today()))) < int(re.sub(r'-', '', date_)) < int(re.sub(r'-', '', str(date.today()))) + 10000
+        today = date.today()
+        return Parser.date_to_int(today) < Parser.date_to_int(date_) < Parser.date_to_int(today) + 10000
 
-    def construct_url(self):
-        """ Construct url """
-        url = 'https://www.flyniki.com/ru/booking/flight/vacancy.php?departure={0}&destination' \
-              '={1}&outboundDate={2}'
-        url_add = '&returnDate={0}'
-        url_oneway = '&oneway={0}&openDateOverview=0' \
-                     '&adultCount=1'
-        self.url = url.format(self.args.outbound, self.args.return_, self.args.departure_date) +\
-                   url_add.format(self.args.return_date) + url_oneway.format(0) \
-            if self.args.return_date != '' else url.format(self.args.outbound,
-                                                           self.args.return_,
-                                                           self.args.departure_date) + url_oneway.format(1)
-
-    def get_page(self):
-        """ Get page """
-        with requests.session() as sess:
-            request = sess.get(self.url)
-            post = sess.post(request.url, data=self.data)
-            self.page = post.text
+    def form_options(self):
+        """ Form options """
+        self.options = {'departure': self.args.outbound,
+                        'destination': self.args.return_,
+                        'outboundDate': self.args.departure_date,
+                        'oneway': '' if self.args.return_date != '' else 1,
+                        'returnDate': self.args.return_date,
+                        'openDateOverview': '0',
+                        'adultCount': 1}
 
     def get_html(self):
-        """ Get HTML """
-        self.html = re.sub(r'\\', '', eval(self.page)['templates']['main'])
+        """ Get page """
+        with requests.session() as sess:
+            request = sess.get('https://www.flyniki.com/ru/booking/flight/vacancy.php', params=self.options)
+            post = sess.post(request.url, data=self.data)
+            self.html = post.json()['templates']['main']
 
     def parse_html(self, xpath):
         """ Parse HTML """
@@ -96,25 +95,21 @@ class Parser:
     def get_line(self, way="outbound"):
         """ Get line from HTML """
         j = 1
-        while True:
+        emptiness_counter = 0
+        while emptiness_counter != 4:
             for i in range(5, 9):
                 xpath = 'string(//div[@class="{0} block"]//table/tbody/tr[{1}]/td[{2}]/label/div/span/@title)'
                 line = self.parse_html(xpath.format(way, 2*j - 1, i))
-                self.lines[way].append(line)
+                if line == '':
+                    emptiness_counter += 1
+                else:
+                    emptiness_counter = 0
+                    self.lines[way].append(line)
             j += 1
-            last = len(self.lines[way]) - 1
-            if last > 2:
-                if self.lines[way][last] == '' and\
-                                self.lines[way][last - 1] == '' and\
-                                self.lines[way][last - 2] == '' and\
-                                self.lines[way][last - 3] == '':
-                    break
-
 
 
 if __name__ == "__main__":
-    flights = Parser()
-    print("Outbound:\n" + '\n'.join([x for x in flights.lines['outbound'] if x != '']))
-    if flights.args.return_date != '':
-        print("Return:\n" + '\n'.join([x for x in flights.lines['return'] if x != '']))
-
+    FLIGHT = Parser()
+    print("Outbound:\n" + '\n'.join(FLIGHT.lines['outbound']))
+    if FLIGHT.args.return_date != '':
+        print("Return:\n" + '\n'.join(FLIGHT.lines['return']))
