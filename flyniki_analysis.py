1e75e02d5557902d3a2f9dc91c03e919d9b69631
Ilia Kurbatovsky <kurbatovsky@gmail.com>
2017-08-17 17:12:16 +0300

    Story 44: Fix struct and names

--- flyniki_analysis.py
+++ flyniki_analysis.py
@@ -13,35 +13,39 @@ from lxml import html
 class Parser(object):
     """ Parser class """
 
-    html = ''
-
     def __init__(self):
         """ Initialization """
-        self.args = self.parse_args()
-        self.options = {}
+        self.args = ''
+        self.tree = ''
         self.currency = ''
-        self.check_args()
         self.lines = {'outbound': [], 'return': []}
         self.flights_combinations = []
-        self.form_options()
+        self.init_variables()
+
+    def init_variables(self):
+        self.args = self.parse_args()
+        self.check_args()
         try:
             self.get_html()
-            self.get_line()
-            self.get_line(way='return')
-            self.get_flights_combinations()
-            self.set_total_cost()
-            self.clean_line_from_combinations()
-            self.get_currency()
         except KeyError:
             print("Flights not found")
             exit(0)
+        self.get_line()
+        if self.args.return_date != '':
+            self.get_line(way='return')
+        self.get_flights_combinations()
+        self.set_total_cost()
+        self.clean_line_from_combinations()
+        self.get_currency()
+
+
 
     @staticmethod
     def parse_args():
         """ Parse args """
         parser = argparse.ArgumentParser()
-        parser.add_argument('outbound', help="start IATA")
-        parser.add_argument('return_', help="finish IATA")
+        parser.add_argument('outbound_airport', help="start IATA")
+        parser.add_argument('return_airport', help="finish IATA")
         parser.add_argument('departure_date', help="Departure date")
         parser.add_argument('return_date', nargs='?', help="Return date", default='')
         return parser.parse_args()
@@ -50,10 +54,10 @@ class Parser(object):
         """ Clean args """
         try:
             assert re.match(r'^[A-Z]{3}$',
-                            self.args.outbound).group() == self.args.outbound, "IATA must be is AAA format"
+                            self.args.outbound_airport).group() == self.args.outbound_airport, "IATA must be is AAA format"
             assert re.match(
                 r'^[A-Z]{3}$',
-                self.args.return_).group() == self.args.return_, "IATA must be is AAA format"
+                self.args.return_airport).group() == self.args.return_airport, "IATA must be is AAA format"
         except AttributeError:
             raise AssertionError, "IATA must be is AAA format"
         assert self.is_correct_date(self.args.departure_date), "Incorrect departure date"
@@ -67,12 +71,12 @@ class Parser(object):
         try:
             return today < datetime.strptime(flight_date, "%Y-%m-%d").date() < today + timedelta(days=355)
         except ValueError:
-            return False
+            raise ValueError, "IATA must be is AAA format"
 
     def form_options(self):
         """ Form options """
-        return {'departure': self.args.outbound,
-                'destination': self.args.return_,
+        return {'departure': self.args.outbound_airport,
+                'destination': self.args.return_airport,
                 'outboundDate': self.args.departure_date,
                 'oneway': '' if self.args.return_date != '' else 1,
                 'returnDate': self.args.return_date,
@@ -82,8 +86,8 @@ class Parser(object):
     def get_html(self):
         """ Get page """
         data = {'_ajax[templates][]': 'main',
-                '_ajax[requestParams][departure]': self.args.outbound,
-                '_ajax[requestParams][destination]': self.args.return_,
+                '_ajax[requestParams][departure]': self.args.outbound_airport,
+                '_ajax[requestParams][destination]': self.args.return_airport,
                 '_ajax[requestParams][outboundDate]': self.args.departure_date,
                 '_ajax[requestParams][returnDate]': self.args.return_date,
                 '_ajax[requestParams][adultCount]': '1',
@@ -97,13 +101,12 @@ class Parser(object):
         with requests.session() as sess:
             request = sess.get(url, params=self.form_options())
             post = sess.post(request.url, data=data)
-            self.html = post.json()['templates']['main']
+            self.tree = html.fromstring(post.json()['templates']['main'])
 
     def get_line(self, way="outbound"):
         """ Get line from HTML """
-        tree = html.fromstring(self.html)
         xpath = '//div[@class="{0} block"]//tr[@role="group"]'
-        for tr in tree.xpath(xpath.format(way)):
+        for tr in self.tree.xpath(xpath.format(way)):
             self.lines[way].extend(tr.xpath('td[@role="radio"]//label/div[@class="lowest"]/span/@title'))
         self.lines[way] = map(lambda x: re.sub(r'\.', '', x), self.lines[way])
         if not self.lines[way]:
@@ -118,17 +121,17 @@ class Parser(object):
             return 0
 
     @staticmethod
-    def get_price_from_tuple(tup):
+    def get_price_from_tuple(tuple_with_flights):
         """ Function for sort """
-        return float(tup[2])
+        return float(tuple_with_flights[2])
 
     def get_flights_combinations(self):
         """ Return all combinations of outbound and return flights """
         self.flights_combinations = list(itertools.product(self.lines['outbound'], self.lines['return']))
 
-    def calculate_total_cost(self, tup):
+    def calculate_total_cost(self, tuple_with_flights):
         """ Return total cost of outbound and return flights """
-        return str(self.get_price_from_string(tup[0]) + self.get_price_from_string(tup[1]))
+        return str(self.get_price_from_string(tuple_with_flights[0]) + self.get_price_from_string(tuple_with_flights[1]))
 
     def set_total_cost(self):
         """ Set total cost in combinations list """
@@ -146,13 +149,12 @@ class Parser(object):
 
     def get_currency(self):
         """ get currency from HTML """
-        tree = html.fromstring(self.html)
         xpath = '//div[@class="outbound block"]//thead/tr[2]/th[4]/text()'
-        self.currency = tree.xpath(xpath)[0]
+        self.currency = self.tree.xpath(xpath)[0]
 
 if __name__ == "__main__":
     FLIGHT = Parser()
     if FLIGHT.args.return_date != '':
         print("Combinations:\n" + '{0}\n'.join(['  —   '.join(x) for x in sorted(FLIGHT.flights_combinations, key=Parser.get_price_from_tuple)]).format(FLIGHT.currency) + FLIGHT.currency)
     else:
-        print("Outbound:\n" + '{0}\n'.join(sorted(FLIGHT.lines['outbound'], key=Parser.get_price_from_string)).format(FLIGHT.currency) + FLIGHT.currency)
+        print(re.sub(r',0', '.', "Outbound:\n" + '{0}\n'.join(sorted(FLIGHT.lines['outbound'], key=Parser.get_price_from_string)).format(FLIGHT.currency) + FLIGHT.currency))
