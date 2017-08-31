9dc015c406e1e24bcf1a934aaded6cd93492de1e
Ilia Kurbatovsky <kurbatovsky@gmail.com>
2017-08-31 17:42:26 +0300

    Story 63: Upgrade flyniki_existing_flights.py

--- flyniki_existing_flights.py
+++ flyniki_existing_flights.py
@@ -7,26 +7,37 @@ import argparse
 import requests
 
 
-def save_in_file(a_function_to_decorate):
-    def wrapper(*args):
-        result = a_function_to_decorate(*args)
-        with open(os.path.join(os.path.abspath(os.curdir), 'Functions', '{}.txt'.format(a_function_to_decorate.__name__)), 'a') as function_file:
-            function_file.write(str(result))
-            function_file.write('\n')
-        return result
-    return wrapper
+def save(type="print", file_name=''):
+    def my_decorator(a_function_to_decorate):
+        def wrapper(*args):
+            result = a_function_to_decorate(*args)
+            if type == "file":
+                with open(os.path.join(os.path.abspath(os.curdir), 'Functions', '{}.txt'.format(
+                        a_function_to_decorate.__name__ if not file_name else file_name)), 'a') as function_file:
+                    function_file.write(str(result))
+                    function_file.write('\n')
+            elif type == "print":
+                print(result)
+            else:
+                raise AttributeError
+
+            return result
+        return wrapper
+    return my_decorator
+
+
 
 
 class AllFlights(object):
     """ Get all flights for each airports from flyniki """
 
-    @save_in_file
+    @save(type="print")
     def __init__(self):
         """ Object initialization """
         self.all_airports = []
         self.all_ways = {}
 
-    @save_in_file
+    @save(type="print")
     def get_airports(self):
         """ Get airports"""
         with requests.session() as sess:
@@ -35,7 +46,7 @@ class AllFlights(object):
                 self.all_ways[iata] = self.get_airports_from_json(sess, self.form_options(iata))
 
     @staticmethod
-    @save_in_file
+    @save(type="print")
     def get_airports_from_json(sess, params):
         """ Get airports from json """
         url = 'https://www.flyniki.com/ru/site/json/suggestAirport.php'
@@ -47,7 +58,7 @@ class AllFlights(object):
         return result
 
     @staticmethod
-    @save_in_file
+    @save(type="print")
     def form_options(code=''):
         """ For options """
         return {'searchfor': 'destinations' if code else 'departures',
@@ -63,13 +74,13 @@ class AllFlights(object):
                 'routesource[0]': 'airberlin',
                 'routesource[1]': 'partner'}
 
-    @save_in_file
+    @save(type="print")
     def check_way(self, info):
         self.get_airports()
         all_ways = self.list_to_dict()
         return info.return_airport in all_ways[info.outbound_airport]
 
-@save_in_file
+@save(type="print")
 def output(ways):
     """ Output function """
     for way in ways.keys():
@@ -77,14 +88,14 @@ def output(ways):
         print(' '.join(ways[way]))
         print('\n\n')
 
-@save_in_file
+@save(type="print")
 def write_in_files(ways):
     """ Write ways in files"""
     for way in ways.keys():
         with open(os.path.join(os.path.abspath(os.curdir), 'Flights', '{}.txt'.format(way)), 'a') as flights_file:
             flights_file.writelines('\n'.join(ways[way]))
 
-@save_in_file
+@save(type="print")
 def refresh():
     """ Refresh info """
     flights = AllFlights()
@@ -92,7 +103,7 @@ def refresh():
     write_in_files(flights.all_ways)
     output(flights.all_ways)
 
-@save_in_file
+@save(type="print")
 def parse_args():
     """ Parse args """
     parser = argparse.ArgumentParser()
@@ -100,7 +111,7 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-@save_in_file
+@save(type="print")
 def main():
     """ Main function """
     if parse_args().refresh:
